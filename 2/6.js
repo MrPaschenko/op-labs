584b9539250b59df01fe541fa385cdcf4922651e
MrPaschenko <paschenkod2@gmail.com>
2021-01-03 01:48:20 +0200

    2.6

---
+++ 2/6.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function average(a, b) {
+  return (a + b) / 2;
+}
+
+function square(x) {
+  return Math.pow(x, 2);
+}
+
+function cube(x) {
+  return Math.pow(x, 3);
+}
+
+function calculate(max) {
+  const array = [];
+  for (let i = 0; i < max; i++) {
+    array[i] = average(square(i), cube(i));
+  }
+  return array;
+}
+
+console.log(calculate(9));
