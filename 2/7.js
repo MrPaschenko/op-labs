fe304b9fe38f82d38cb328e86b6de7fe15747601
MrPaschenko <paschenkod2@gmail.com>
2021-01-05 01:18:33 +0200

    2.7

--- 2/7.js
+++ 2/7.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const fn = null;
+const person1 = {
+  name: 'Kirill',
+};
 
-module.exports = { fn };
+let person2 = {
+  name: 'Marcus',
+};
+
+person1.name = 'notKirill'; // new value for person1.name
+person2.name = 'notMarcus'; // new value for person2.name
+
+console.dir({ person1, person2 }); // output
+
+// person1 = {   // cannot change object because of const
+//   age: 22,    // cannot change object because of const
+// };            // cannot change object because of const
+
+person2 = {      // object is changeable because of let
+  age: 15,       // object is changeable because of let
+};               // object is changeable because of let
+
+console.dir({ person1, person2 }); // output
