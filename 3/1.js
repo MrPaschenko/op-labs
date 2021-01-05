6f5390841fa0fb9f8179199a98a50993ed92ebd9
MrPaschenko <paschenkod2@gmail.com>
2021-01-05 02:35:15 +0200

    3.1

---
+++ 3/1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function random(min, max = undefined) {
+  if (max === undefined) {
+    return Math.round(Math.random() * min);
+  } else
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+console.log(random(0, 6));
+
+console.log(random(6));
