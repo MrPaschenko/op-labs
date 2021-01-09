c5fbc314b23722c9d6a5961a5b74666e7334a588
MrPaschenko <paschenkod2@gmail.com>
2021-01-09 23:54:32 +0200

    6.2

--- 6-Composition/2-compose.js
+++ 6-Composition/2-compose.js
@@ -1,5 +1,21 @@
 'use strict';
 
 const compose = (...fns) => x => {
-
+  try {
+    return fns.reduceRight((v, f) => f(v), x);
+  } catch (err) {
+    fns.pop();
+    return fns.reduceRight((v, f) => f(v), x);
+  }
 };
+
+const error = () => {
+  throw new Error('hello, its me');
+};
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+const f = compose(cube, twice, inc, error);
+const x = f(5);
+console.log(x);
