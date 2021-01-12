589ac05ca03b92d28ddab39c3f693e3e94056b15
MrPaschenko <paschenkod2@gmail.com>
2021-01-12 21:56:01 +0200

    console output comments

--- 5-Closure/1-seq.js
+++ 5-Closure/1-seq.js
@@ -2,6 +2,6 @@
 
 const seq = f => g => (typeof(g) === 'number' ? f(g) : seq(x => f(g(x))));
 
-console.log(seq(x => x + 7)(x => x * 2)(x => 5 + x)(6));
-console.log(seq(x => x * 2)(x => x + 7)(5));
-console.log(seq(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7));
+console.log(seq(x => x + 7)(x => x * 2)(x => 5 + x)(6)); // 29
+console.log(seq(x => x * 2)(x => x + 7)(5)); // 24
+console.log(seq(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7)); // 3
--- 5-Closure/2-array.js
+++ 5-Closure/2-array.js
@@ -14,12 +14,12 @@ arr.push('first');
 arr.push('second');
 arr.push('third');
 
-console.log(arr(0)); // Выводит: first
-console.log(arr(1)); // Выводит: second
-console.log(arr(2)); // Выводит: third
+console.log(arr(0)); // 'first'
+console.log(arr(1)); // 'second'
+console.log(arr(2)); // 'third'
 
-console.log(arr.pop()); // Выводит: third
-console.log(arr.pop()); // Выводит: second
-console.log(arr.pop()); // Выводит: first
+console.log(arr.pop()); // 'third'
+console.log(arr.pop()); // 'second'
+console.log(arr.pop()); // 'first'
 
-console.log(arr.pop()); // Выводит: undefined
+console.log(arr.pop()); // 'undefined'
