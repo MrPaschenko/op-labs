afca354844511e38c76d7d1786dc7d5e16a3e69c
MrPaschenko <paschenkod2@gmail.com>
2021-01-13 18:14:42 +0200

    8.1

--- 8-HigherOrderFunction/1-callback.js
+++ 8-HigherOrderFunction/1-callback.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function iterate(object, callback) {
-
+  for (const key in object) {
+    callback(key, object[key], object);
+  }
 }
+
+const obj = { a: 1, b: 2, c: 3 };
+iterate(obj, (key, value) => {
+  console.log({ key, value });
+});
+
+// { key: 'a', value: 1 }
+// { key: 'b', value: 2 }
+// { key: 'c', value: 3 }
