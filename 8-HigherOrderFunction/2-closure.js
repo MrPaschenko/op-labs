a74c3bdf447883c7ed3fb6284c2d1c33e17a0463
MrPaschenko <paschenkod2@gmail.com>
2021-01-13 18:55:16 +0200

    lambda function instead of function declaration

--- 8-HigherOrderFunction/2-closure.js
+++ 8-HigherOrderFunction/2-closure.js
@@ -1,9 +1,11 @@
 'use strict';
 
-function store(value) {
-  const storage = [value];
-  return () => storage.shift();
-}
+// function store(value) {
+//   const storage = [value];
+//   return () => storage.shift();
+// }
+
+const store = value => () => value;
 
 const read = store(5);
 const value = read();
